bf7498a13dbec6751c5adcfee456fa0536b75899
Sven Johansson <johansson.sven@gmail.com>
2026-05-19 23:29:49 +0200

    libpixils: Added benchmark for content size cache

--- lib/pixils/benchmark/support/benchmark.h
+++ lib/pixils/benchmark/support/benchmark.h
@@ -21,7 +21,7 @@ namespace Pixils::Benchmark
     bool allow_dirty_worktree = false;
     std::string output_dir = "benchmarks/pixils";
     // Change this manually when starting a new benchmark goalpost.
-    std::string goalpost = "006_effective_style_as_view_source_of_truth_8b42848";
+    std::string goalpost = "007_natural_content_size_cache_7cdd4b2";
     std::string run_name = "history";
     std::vector<std::string> category_filter;
   };
