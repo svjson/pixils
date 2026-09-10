86e346f3d960f071e2e3faab47764686b2de7237
Sven Johansson <johansson.sven@gmail.com>
2026-09-10 20:30:53 +0200

    pixils-tilemap: Adapt to roo ABI change

--- lib/pixils-tilemap/src/native/tilemap_native.cpp
+++ lib/pixils-tilemap/src/native/tilemap_native.cpp
@@ -3756,6 +3756,7 @@ namespace
 
   int load_native_package(const RooNativeHostV1* host)
   {
+    package_last_error.clear();
     try
     {
       auto ns = std::make_unique<RenderImplNamespace>();
@@ -3785,11 +3786,6 @@ namespace
     }
   }
 
-  void unload_native_package()
-  {
-    package_last_error.clear();
-  }
-
   const char* last_error()
   {
     return package_last_error.c_str();
@@ -3805,7 +3801,6 @@ extern "C" ROO_NATIVE_EXPORT const RooNativePackageV1* roo_native_package_v1()
     "0.1.0",
     ROO_NATIVE_CXX_ABI,
     load_native_package,
-    unload_native_package,
     last_error,
   };
   return &package;
